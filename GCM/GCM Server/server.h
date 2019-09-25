b631392032a805f775c94b378033d934a59bd4be
Maziyar <maziyar.nazary@gmail.com>
2019-09-25 08:07:30 -0600

    Server code for initialization of cgroups added. server code now can reclaim memory from agents. some bug fixes

--- GCM/GCM Server/server.h
+++ GCM/GCM Server/server.h
@@ -16,15 +16,25 @@
 
 #define true 1
 #define false 0
+#define _init_ 2
 #define __PORT__ 4444
 #define __MAX_CLIENT__ 30
+#define __MAXSERVERS__ 10
 #define __BUFFSIZE__ 16
 #define __FAILED__ -1
 #define __NOMEM__ -2
 #define __QUOTA__ 5000
 
+int cgroup_fds[__MAX_CLIENT__];
 unsigned long memory_limit = 30000;
 unsigned long mem_reqs = 0;
+int num_of_clients = 0;
+int num_of_agents = 0;
+
+
+
+int cgroups_root_pid[__MAX_CLIENT__];
+pthread_mutex_t mlock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef int _bool;
 
@@ -47,14 +57,59 @@ typedef struct ec_msg {
 
 } ec_message_t;
 
-unsigned long handle_req(char* buffer);
+typedef struct ec_reclaim_msg {
 
-void *handle_client_reqs(void* thread_id);
+	unsigned short cgroup_id;
+
+	int is_mem;
+
+	//...maybe it needs more things
+
+} ec_reclaim_msg_t;
+
+typedef struct ec_cli
+{
+	struct sockaddr_in* cliaddr;
+
+	int cgroup_fd;
+
+	unsigned short cgroup_id;
+	//... Maybe it will need more info. Not now
+} ec_client_t;
+
+ec_client_t* ec_cgroups[__MAX_CLIENT__];
+
+typedef struct serv_thread_args
+{
+	int clifd;
+
+	struct sockaddr_in* cliaddr;
+}serv_thread_args_t;
+
+typedef struct agent
+{
+	char* ip;
+	
+	int port;
+
+	int sockfd;
+
+}agent_t;
+
+agent_t* ec_agents[__MAXSERVERS__];
+
+unsigned long handle_req(char* buffer, int client_fd);
+
+void *handle_client_reqs(void* args);
 
 gcm_server_t* create_mt_server();
 
-unsigned long handle_mem_req(ec_message_t* req);
+unsigned long handle_mem_req(ec_message_t* req, int client_fd);
 
-unsigned long handle_mem_req(ec_message_t* req);
+unsigned long reclaim_memory(int client_fd);
+
+void add_cgroup_id_to_ec(ec_message_t* req, int client_fd);
+
+void init_agents_connection(int num_of_agents);
 
 #endif
