837db0a797ddb976b1fbe9464c042017e5866024
Greg Cusack <gregorycusack@gmail.com>
2019-11-20 14:09:21 -0700

    add variable port # in sys_connenect. pass in as cmdline arg

--- ec_docker/sat-cpu-ec.sh
+++ ec_docker/sat-cpu-ec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 CONTAINER_NAME=$1
-PORT=$2
-QUOTA=$(($3 * 1000))
+IP=$2
+PORT=$3
+QUOTA=$(($4 * 1000))
 echo $QUOTA
 
 CONTAINER_ID="$(sudo docker run -d --cpu-period=100000 --cpu-quota=$QUOTA --cpuset-cpus 0 "$CONTAINER_NAME")"
@@ -11,6 +12,6 @@ echo "container id: "$CONTAINER_ID""
 echo "container init pid: "$CONTAINER_PID""
 
 export KERNEL_HOME="../"
-../ec_syscalls/sys_connect "$CONTAINER_PID" "$PORT" "$EC_ID"
+../ec_syscalls/sys_connect "$IP" "$CONTAINER_PID" "$PORT"
 
 export CONTAINER_ID
