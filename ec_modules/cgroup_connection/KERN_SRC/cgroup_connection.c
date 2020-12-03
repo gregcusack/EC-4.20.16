0b8b90d2a6cbfe4a9ed7ceabca83dd101d7f1053
greg <gregorycusack@gmail.com>
2020-12-02 19:23:07 -0700

    reset resize_max_mem and return before sending cpu usage

--- ec_modules/cgroup_connection/KERN_SRC/cgroup_connection.c
+++ ec_modules/cgroup_connection/KERN_SRC/cgroup_connection.c
@@ -59,8 +59,10 @@ int tcp_send(struct socket* sock, const char* buff, const size_t length, unsigne
 		if(len > 0){
 			written += len;
 			left -= len;
-			if(left)
+			if(left) {
+				printk(KERN_ALERT "[ERROR]: repeat send!\n");
 				goto repeat_send;
+			}
 
 		}
 	set_fs(oldmm);
@@ -125,7 +127,7 @@ int report_cpu_usage(struct cfs_bandwidth *cfs_b){
 	if (!cfs_b) {
 		printk(KERN_ERR "[EC ERROR] report_cpu_usage(): cfs_b == NULL...idk what to do\n");
 		ret = -1;
-		// goto failed;
+		goto failed;
 	}
 
 	serv_req = (ec_message_t*) kmalloc(sizeof(ec_message_t), GFP_KERNEL);
@@ -138,21 +140,21 @@ int report_cpu_usage(struct cfs_bandwidth *cfs_b){
 	serv_req -> runtime_remaining 	= cfs_b->runtime;
 	sockfd 							= cfs_b->ecc->ec_cli;
 
+	// kfree(serv_req);
+	// return 0;
+	//printk(KERN_ERR "[EC TX INFO]: (%d, %d, %lld, %d, %lld)\n", serv_req->cgroup_id, serv_req->req_type, serv_req->rsrc_amnt, serv_req->request, serv_req->runtime_remaining);
+
+//	spin_lock(&sock_lock);
+	ret = tcp_send(sockfd, (char*)serv_req, sizeof(ec_message_t), MSG_DONTWAIT);
+//	spin_unlock(&sock_lock);
+
+	if(ret) {
+		printk(KERN_INFO "TX failed\n");
+	}
 	kfree(serv_req);
-	return 0;
-// 	//printk(KERN_ERR "[EC TX INFO]: (%d, %d, %lld, %d, %lld)\n", serv_req->cgroup_id, serv_req->req_type, serv_req->rsrc_amnt, serv_req->request, serv_req->runtime_remaining);
 
-// //	spin_lock(&sock_lock);
-// 	ret = tcp_send(sockfd, (char*)serv_req, sizeof(ec_message_t), MSG_DONTWAIT);
-// //	spin_unlock(&sock_lock);
-
-// 	if(ret) {
-// 		printk(KERN_INFO "TX failed\n");
-// 	}
-// 	kfree(serv_req);
-
-// failed:
-// 	return ret;
+failed:
+	return ret;
 }
 
 unsigned long request_memory(struct mem_cgroup *memcg){
